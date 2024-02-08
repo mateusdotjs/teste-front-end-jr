352b062cdab1e2b0d9b9020be7d51cdd41c224e1
Mateus <mateus.silvainfo@gmail.com>
2024-02-07 22:18:47 -0300

    added button props type checking

--- src/components/carousel/NextArrow.tsx
+++ src/components/carousel/NextArrow.tsx
@@ -1,7 +1,8 @@
+import { ComponentProps } from "react";
 import Next from "../../assets/icons/carousel/right-arrow.svg?react";
 import "./carousel.scss";
 
-const NextArrow = ({ onClick }) => {
+const NextArrow = ({ onClick }: ComponentProps<'button'>) => {
   return (
     <button className="carousel-button right-arrow" onClick={onClick}>
       <Next />
--- src/components/carousel/PrevArrow.tsx
+++ src/components/carousel/PrevArrow.tsx
@@ -1,7 +1,8 @@
+import { ComponentProps } from "react";
 import Prev from "../../assets/icons/carousel/left-arrow.svg?react";
 import "./carousel.scss";
 
-const PrevArrow = ({ onClick }) => {
+const PrevArrow = ({ onClick }: ComponentProps<'button'>) => {
   return (
     <button className="carousel-button left-arrow" onClick={onClick}>
       <Prev />
